b0ccf40428bd7b0f4f78d8ad3301d191ae18d8a9
Kai Friis <kai.friis@bi.no>
2018-09-24 22:30:00 +0200

    My initial commit

---
+++ RetailSystem.Tests/CashRegisterTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RetailSystem.Tests
+{
+    public class CashRegisterTests
+
+    {
+        [TestCase(1, 60)]
+        [TestCase(2, 120)]
+        [TestCase(3, 120)]
+        [TestCase(4, 180)]
+        [TestCase(5, 240)]
+        [TestCase(6, 240)]
+        [TestCase(7, 300)]
+        [Test]
+        public void AddPairsOfRubberGloves_returnsKr(int number, int totalPrice)
+        {
+            var cashRegister = new RetailPayment();
+            cashRegister.Add("PLU A", number);
+            var totalCost = cashRegister.CalculateCost();
+            Assert.That(totalCost, Is.EqualTo(totalPrice));
+        }
+
+
+        [TestCase(1, 399)]
+        [TestCase(2, 798)]
+        [TestCase(3, 999)]
+        [TestCase(4, 1398)]
+        [Test]
+        public void AddNumbersOfStethoscopes_returnsKr(int number, int totalPrice)
+        {
+            var cashRegister = new RetailPayment();
+            cashRegister.Add("PLU B", number);
+            var totalCost = cashRegister.CalculateCost();
+            Assert.That(totalCost, Is.EqualTo(totalPrice));
+        }
+
+    }
+}
---
+++ RetailSystem/RetailPayment.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailSystem
+{
+    public class RetailPayment
+    {
+
+        private int _pairOfPluA;
+        private int _numberOfPluB;
+        private double _amountOfPluC;
+
+        private const double PricePluA = 59.90;
+        private const int PricePluB = 399;
+        private const int PriceThreePackPluB = 999;
+        public RetailPayment()
+        {
+            _pairOfPluA = 0;
+            _numberOfPluB = 0;
+            _amountOfPluC = 0;
+        }
+
+        public void Add(string plu, int i)
+        {
+            switch (plu)
+            {
+                case "PLU A":
+                    _pairOfPluA += i;
+                    break;
+                case "PLU B":
+                    _numberOfPluB += i;
+                    break;
+                case "PLU C":
+                    _amountOfPluC += i;
+                    break;
+                default:
+                    //Error: Unknown product
+                    break;
+            }
+        }
+
+        public int CalculateCost()
+        {
+            var numberPluAPayingFor = _pairOfPluA - _pairOfPluA / 3;
+            var pricePluA = numberPluAPayingFor * PricePluA;
+            var pricePluB = CalculatePluBCost(_numberOfPluB, PricePluB, PriceThreePackPluB);
+
+            var price = pricePluA + pricePluB;
+
+            return (RoundOff(price));
+        }
+
+        private static int CalculatePluBCost(int numberOfArticles, int pricePrArticle, int bundlePrice)
+        {
+            const int amountIsBundle = 3;
+            var numberOfBundles = numberOfArticles / amountIsBundle;
+            var rest = numberOfArticles % amountIsBundle;
+            return numberOfBundles * bundlePrice + rest * pricePrArticle;
+        }
+
+
+        private int RoundOff(double price)
+        {
+            return (int) Math.Round(price, MidpointRounding.AwayFromZero);
+        }
+    }
+
+}
